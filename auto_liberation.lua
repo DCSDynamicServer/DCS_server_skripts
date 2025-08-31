0abe6feeb16245102d7a84c8627c84566de8ea31
DCSDynamicServer <marcdenis.schulze@gmail.com>
2025-08-31 16:34:48 +0200

    Update auto_liberation.lua
    v0.03

--- auto_liberation.lua
+++ auto_liberation.lua
@@ -1,22 +1,28 @@
+local logfile = io.open("C:\\DCS Server Liberation\\dcs_liberation.13.0.0\\auto_liberation.log", "a")
+
+local function log(msg)
+    net.log(msg)
+    if logfile then
+        logfile:write(os.date("[%Y-%m-%d %H:%M:%S] ") .. msg .. "\n")
+        logfile:flush()
+    end
+end
+
 local function load_next()
-    net.log("Liberation AutoLoader: Lade next.miz ...")
     local miz = "C:\\DCS Server Liberation\\dcs_liberation.13.0.0\\next.miz"
     if lfs.attributes(miz) then
+        log("Liberation AutoLoader: Lade " .. miz)
         net.load_mission(miz)
-        net.log("Liberation AutoLoader: Mission geladen -> " .. miz)
     else
-        net.log("Liberation AutoLoader: Konnte next.miz nicht finden!")
+        log("Liberation AutoLoader: Konnte next.miz nicht finden!")
     end
 end
 
 local function on_end()
-    net.log("Liberation AutoLoader: Mission beendet, starte Liberation-CLI ...")
-    -- Batch-Skript ausführen
+    log("Liberation AutoLoader: Mission beendet, starte Liberation-CLI ...")
     os.execute('cmd /c "C:\\DCS Server Liberation\\dcs_liberation.13.0.0\\liberation_next_turn.bat"')
-    -- Wartezeit (40 Sekunden), damit Liberation genug Zeit hat, die neue .miz zu schreiben
-    net.log("Liberation AutoLoader: Warte 40 Sekunden bevor next.miz geladen wird ...")
+    log("Liberation AutoLoader: Warte 40 Sekunden, bevor next.miz geladen wird ...")
     os.execute("ping -n 40 127.0.0.1 > nul")
-    -- Neue Mission laden
     load_next()
 end
 
